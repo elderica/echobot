a1fe670ef924252024f9eefc698b4b7986152195
Kazuki Shigemichi <shigemichik@gmail.com>
2018-10-10 02:16:40 +0900

    implement echoback

--- lib/Echobot/Controller/Webhook.pm
+++ lib/Echobot/Controller/Webhook.pm
@@ -1,6 +1,11 @@
 package Echobot::Controller::Webhook;
 use Mojo::Base 'Mojolicious::Controller';
 
+use Mojo::UserAgent;
+use Mojo::JSON 'decode_json';
+use Mojo::JSON::Pointer;
+use Mojo::Util 'dumper';
+
 use Echobot::Util qw(verify_line_signature);
 
 sub webhook {
@@ -10,6 +15,7 @@ sub webhook {
   my $log = $app->log;
 
   my $channel_secret = $app->config('channel_secret');
+  my $channel_access_token = $app->config('channel_access_token');
 
   $log->fatal('channel_secret is not set in config')
     unless defined($channel_secret);
@@ -28,6 +34,33 @@ sub webhook {
       ? 'signature is ok'
       : 'signature is bad'
   );
+
+  my $decoded = decode_json($req_body);
+  my $pointer = Mojo::JSON::Pointer->new($decoded);
+  my $events = $pointer->get('/events');
+
+  $log->debug(dumper $events);
+
+  my $ua = Mojo::UserAgent->new;
+
+  for my $event (@{$events}) {
+    my $reply_token = $event->{replyToken};
+    my $text = $event->{message}->{text};
+
+    $ua->post(
+      'https://api.line.me/v2/bot/message/reply'
+      => {Authorization  => "Bearer $channel_access_token"}
+      => json => {
+        replyToken => $reply_token,
+        messages   => [
+          {
+            type => 'text',
+            text => $text
+          }
+        ]
+      }
+    );
+  }
 
   $self->render(data => '');
 }
